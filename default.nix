3f028bf809536e8b15e2c237a4691a348c347c3b
j-keck <jhyphenkeck@gmail.com>
2018-01-14 20:20:48 +0100

    travis script to build the website
      * currently no deployment

---
+++ default.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.stdenv.mkDerivation rec {
+  src = pkgs.lib.cleanSource ./.;
+  name = "website";
+
+  buildInputs = [
+    pkgs.python27Packages.lektor
+  ];
+
+  installPhase = ''
+    mkdir -p $out
+    lektor build -O $out
+  '';
+
+  shellHook = ''
+    lektor server -h 0.0.0.0
+  '';
+}
