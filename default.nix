ef632f4da55b8126d31a14a7b573c28254cc691a
j-keck <jhyphenkeck@gmail.com>
2019-04-25 18:08:14 +0200

    pin nixpkgs
      - travis uses nixpkgs-unstable by default
    
      - lektor on nixpkgs-unstable currently fails with:
        TypeError: decode() argument 1 must be string, not None
    
      - pin nixpkgs to the actual stable nixos-19.03 release

--- default.nix
+++ default.nix
@@ -1,6 +1,17 @@
-{ pkgs ? import <nixpkgs> {} }:
+let
 
-pkgs.stdenv.mkDerivation rec {
+  fetchNixpkgs = { rev, sha256 }: builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    inherit sha256;
+  };
+
+  pkgs = import (fetchNixpkgs {
+    # pin nixos-19.03: 2019-04-24T11:23:05+02:00
+    rev = "2f1eacc949fe31f34d7af4ebfd97c01a02ceee16";
+    sha256 = "1nvx87xc1yiv6zkp34mdwhxrgsg4idncpy7zvc7cs3qw918hflfs";
+  }) {};
+
+in pkgs.stdenv.mkDerivation rec {
   src = pkgs.lib.cleanSource ./.;
   name = "website";
 
