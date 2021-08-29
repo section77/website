d6d39dd9c3b9f0299a096dde8f3aecb863b75721
Jürgen Keck <jhyphenkeck@gmail.com>
2021-08-29 12:38:15 +0200

    update deps

--- default.nix
+++ default.nix
@@ -1,15 +1,11 @@
 let
 
-  fetchNixpkgs = { rev, sha256 }: builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    inherit sha256;
-  };
-
-  pkgs = import (fetchNixpkgs {
-    # pin nixos-19.03: 2019-04-24T11:23:05+02:00
-    rev = "2f1eacc949fe31f34d7af4ebfd97c01a02ceee16";
-    sha256 = "1nvx87xc1yiv6zkp34mdwhxrgsg4idncpy7zvc7cs3qw918hflfs";
-  }) {};
+  pkgs = import (builtins.fetchGit {
+    name = "nixos-21.05";
+    url = "https://github.com/nixos/nixpkgs/";
+    ref = "refs/heads/nixos-21.05";
+    rev = "a1007637cea374bd1bafd754cfd5388894c49129";
+    }) {};
 
   site = pkgs.stdenv.mkDerivation rec {
     src = pkgs.lib.cleanSource ./.;
